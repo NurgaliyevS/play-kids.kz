3ff55071a8ee628079adc746ddd89ef6b568cd05
NurgaliyevS <nurgasab@gmail.com>
2025-01-01 13:25:28 +0500

    upd

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,31 +1,31 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
 function ProductCard({ product }) {
   return (
     <div className="group relative">
-      <div className="aspect-square overflow-hidden rounded-lg bg-gray-100">
-        <Image
-          src={product.image}
-          alt={product.name}
-          width={400}
-          height={400}
-          className="h-full w-full object-cover object-center hover:opacity-75"
-        />
-      </div>
-      <div className="mt-4 flex justify-between">
-        <div>
-          <h3 className="text-sm font-medium text-gray-900">
-            <Link href={`/products/${product.id}`}>
-              {product.name}
-            </Link>
-          </h3>
-          <p className="mt-1 text-sm text-gray-500">{product.category}</p>
+      <Link href={`/products/${product.id}`}>
+        <div className="aspect-square overflow-hidden rounded-lg bg-gray-100">
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={400}
+            height={400}
+            className="h-full w-full object-cover object-center hover:opacity-75"
+          />
         </div>
-        <p className="text-sm font-medium text-gray-900">{product.price}</p>
-      </div>
+        <div className="mt-4 flex justify-between">
+          <div>
+            <h3 className="text-sm font-medium text-gray-900">
+              <span>{product.name}</span>
+            </h3>
+            <p className="mt-1 text-sm text-gray-500">{product.category}</p>
+          </div>
+          <p className="text-sm font-medium text-gray-900">{product.price}</p>
+        </div>
+      </Link>
     </div>
-  )
+  );
 }
 
 export default ProductCard;
